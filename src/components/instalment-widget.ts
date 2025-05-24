02cde6a6aa9afb96c70f426509b6c83fd3a23397
Jose Cabrera <joselu11cabrera@gmail.com>
2025-05-24 10:42:05 +0200

    refactor: implement instalment options dynamically using a structured data model

--- src/components/instalment-widget.ts
+++ src/components/instalment-widget.ts
@@ -1,5 +1,24 @@
+type StringValuePair = { value: number; string: string };
+type Instalment = {
+  instalment_count: number;
+  total_with_tax: StringValuePair;
+  instalment_amount: StringValuePair;
+  instalment_fee: StringValuePair;
+  instalment_total: StringValuePair;
+  grand_total: StringValuePair;
+  cost_of_credit: StringValuePair;
+  cost_of_credit_pct: StringValuePair;
+  apr: StringValuePair;
+  max_financed_amount: StringValuePair;
+};
+
 export class InstalmentWidget extends HTMLElement {
   private shadowDOM: ShadowRoot;
+  private readonly instalments: Instalment[] = [
+    createInstalment(3, 5300, '53,00 €'),
+    createInstalment(6, 2800, '28,00 €'),
+    createInstalment(12, 1550, '15,50 €'),
+  ];
 
   constructor() {
     super();
@@ -16,11 +35,59 @@ export class InstalmentWidget extends HTMLElement {
         <label for="instalment-options">Págalo en</label>
         <button type="button">Más info</button>
         <select id="instalment-options">
-          <option value="3">3 cuotas de 53,00 €/mes</option>
-          <option value="6">6 cuotas de 28,00 €/mes</option>
-          <option value="12">12 cuotas de 15,50 €/mes</option>
+          ${this.instalments.map((instalment) => {
+            return `
+                <option 
+                  value="${instalment.instalment_count}">
+                  ${instalment.instalment_count} cuotas de ${instalment.instalment_amount.string}/mes 
+                </option>
+          `;
+          })}
         </select>
       </form>
     `;
   }
+}
+
+function createInstalment(
+  instalmentCount: number,
+  totalWithTax: number,
+  totalWithTaxCentsString: string
+) {
+  return {
+    instalment_count: instalmentCount,
+    total_with_tax: {
+      value: totalWithTax,
+      string: totalWithTaxCentsString,
+    },
+    instalment_amount: {
+      value: totalWithTax,
+      string: totalWithTaxCentsString,
+    },
+    instalment_fee: {
+      value: totalWithTax,
+      string: totalWithTaxCentsString,
+    },
+    instalment_total: {
+      value: totalWithTax,
+      string: totalWithTaxCentsString,
+    },
+    grand_total: {
+      value: totalWithTax,
+      string: totalWithTaxCentsString,
+    },
+    cost_of_credit: {
+      value: totalWithTax,
+      string: totalWithTaxCentsString,
+    },
+    cost_of_credit_pct: {
+      value: totalWithTax,
+      string: totalWithTaxCentsString,
+    },
+    apr: { value: totalWithTax, string: totalWithTaxCentsString },
+    max_financed_amount: {
+      value: totalWithTax,
+      string: totalWithTaxCentsString,
+    },
+  };
 }
